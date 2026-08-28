ba4aca7d5268100b9a0404b4786792146515ec1c
Solomanu Meriga <solomanmeriga33@gmail.com>
2026-08-28 20:05:26 +0530

    feat: add v7 and v8 inside docs/byExtension for GitHub Pages live deployment

---
+++ docs/byExtension/v8/features/download.js
@@ -0,0 +1,29 @@
+export function download(editor, fileName, showToast) {
+  if (!editor) {
+    return;
+  }
+
+  const blob = new Blob(
+    [editor.getValue()],
+    {
+      type: "text/javascript"
+    }
+  );
+
+  const url = URL.createObjectURL(blob);
+
+  const anchor = document.createElement("a");
+
+  anchor.href = url;
+  anchor.download = fileName;
+
+  document.body.appendChild(anchor);
+
+  anchor.click();
+
+  document.body.removeChild(anchor);
+
+  URL.revokeObjectURL(url);
+
+  showToast(`Downloading ${fileName}...`);
+}
